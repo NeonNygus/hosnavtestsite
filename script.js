b88f67cbeab02690641961f42fcdff0c8d79ba13
NeonNygus <92753660+NeonNygus@users.noreply.github.com>
2023-03-07 15:54:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const startPage = document.querySelector("#start-page");
 const chooseCityPage = document.querySelector("#choose-city");
 const chooseHospitalPage = document.querySelector("#choose-hospital");
-const chooseRoomPage = document.querySelector("#choose-room");
+const chooseDoctorPage = document.querySelector("#choose-doctor");
 const trackPage = document.querySelector("#track-page");
+const track = document.querySelector("#track");
 const successPage = document.querySelector("#success-page");
 
 const chooseClinicButton = document.querySelector("#linkChooseClinic");
@@ -12,5 +13,23 @@ chooseClinicButton.addEventListener("click", () => {
 });
 
 function chosenCity() {
-  console.log("dupadupad");
+  chooseCityPage.classList.add("hidden");
+  chooseHospitalPage.classList.remove("hidden");
 }
+
+function chosenHospital() {
+  chooseHospitalPage.classList.add("hidden");
+  chooseDoctorPage.classList.remove("hidden");
+  track.classList.remove("hidden");
+}
+
+function chosenDoctor() {
+  chooseDoctorPage.classList.add("hidden");
+  trackPage.classList.remove("hidden");
+}
+
+track.addEventListener("click", () => {
+  trackPage.classList.add("hidden");
+  track.classList.add("hidden");
+  successPage.classList.remove("hidden");
+});
