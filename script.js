de8884bee1eace0e46cdc9d5b74a5bbd1da06a08
NeonNygus <92753660+NeonNygus@users.noreply.github.com>
2023-03-24 18:15:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 const accX= document.querySelector("#x");
 const accY= document.querySelector("#y");
+const ptrackX = document.querySelector("#trackX");
+const ptrackY = document.querySelector("#trackY");
+let x = 100;
+let y = 100;
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
-
-
-  accelerometer = new Accelerometer({ frequency: 10 });
+function update()
+{
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    x += 1;
+    y += 1;
+    ctx.fillRect(x,y,50,50);
+    requestAnimationFrame(update);
+}
+ update();
+let trackX = 0;
+    let trackY = 0;
+  accelerometer = new Accelerometer({ frequency: 6 });
   accelerometer.onreading = (e) => {
+    
     console.log(e.currentTarget.x);
     accX.innerHTML = e.currentTarget.x;
     accY.innerHTML = e.currentTarget.y;
-    let vx = (e.currentTarget.x) / 2;
-    let vy = (e.currentTarget.y) / 2;
-    function update()
-    {
-        ctx.clearRect(0,0,canvas.width, canvas.height);
-        let x = vx;
-        let y = vy;
-        ctx.fillRect(x,y,50,50);
-        setInterval(requestAnimationFrame(update),1000);
-    }
-    update();
+    vx = (e.currentTarget.x) / 2;
+    vy = (e.currentTarget.y) / 2;
+   trackX += (e.currentTarget.x) / 10;
+   trackY += (e.currentTarget.y) / 10;
+    ptrackX.innerHTML = trackX;
+    ptrackY.innerHTML = trackY;
   };
   accelerometer.start();
-
-
-
-
-
