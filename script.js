3240c9c4d80eac5948de8160d4a727c09c755864
NeonNygus <92753660+NeonNygus@users.noreply.github.com>
2023-03-24 10:16:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const x= document.querySelector("#x");
 const y= document.querySelector("#y");
+let initPos;
+let calibrate = true;
 const sensor = new AbsoluteOrientationSensor({frequency: 60});
 sensor.addEventListener("reading", (e) => 
 {handleSensor(e);
     });
 sensor.start();
-
+document.body.addEventListener("click", () => {calibrate = true})
 function toEuler(q) {
     let sinr_cosp = 2 * (q[3] * q[0] + q[1] * q[2]);
     let cosr_cosp = 1 - 2 * (q[0] * q[0] + q[1] * q[1]);
@@ -18,10 +20,25 @@ function toEuler(q) {
     return [yaw, roll];
 }
 
+function calcDist(angle, initAngle) {
+    angle = (angle - initAngle) * (180 / Math.PI);
+    angle = angle < 0 ? angle + 360 : angle;
+    angle = angle > 180 ? angle - 360 : angle;
+
+    let dist = Math.round(-800 * Math.tan(angle * (Math.PI / 180)));
+    return dist;
+}
+
 function handleSensor(e){
     let quaternion = e.target.quaternion;
     let angles = toEuler(quaternion);
-    x.innerHTML = angles[0];
-    y.innerHTML = angles[1];
-  console.log(angles);
+    
+    if(calibrate){
+        initPos = angles;
+        calibrate = false;
+      }
+    let dist = angles.map((angle, i) => calcDist(angle, initPos[i]));
+      console.log(dist);
+      x.innerHTML = dist[0];
+    y.innerHTML = dist[1];
 }
