126bf78947f60fec8dc7d75e78b7c049ccf60454
NeonNygus <92753660+NeonNygus@users.noreply.github.com>
2023-03-24 16:52:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const accX= document.querySelector("#x");
 const accY= document.querySelector("#y");
-let x = 20;
-let y = 20;
-let vx = 1;
-let vy = 1;
+let x = 100;
+let y = 100;
+let vx = 0;
+let vy = 0;
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 function update()
@@ -21,8 +21,8 @@ function update()
     console.log(e.currentTarget.x);
     accX.innerHTML = e.currentTarget.x;
     accY.innerHTML = e.currentTarget.y;
-    const x = e.currentTarget.x;
-    const y = e.currentTarget.y;
+    vx = (e.currentTarget.x) / 2;
+    vy = (e.currentTarget.y) / 2;
     update();
   };
   accelerometer.start();
