e1b8e1c2a002ed3592d70b53f43d071d523199a8
NeonNygus <92753660+NeonNygus@users.noreply.github.com>
2023-03-24 09:18:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
-const startPage = document.querySelector("#start-page");
-const chooseCityPage = document.querySelector("#choose-city");
-const chooseHospitalPage = document.querySelector("#choose-hospital");
-const chooseDoctorPage = document.querySelector("#choose-doctor");
-const trackPage = document.querySelector("#track-page");
-const track = document.querySelector("#track");
-const successPage = document.querySelector("#success-page");
+const sensor = new AbsoluteOrientationSensor({frequency: 60});
+sensor.addEventListener("reading", (e) => 
+{handleSensor(e);
+    });
+sensor.start();
 
-const chooseClinicButton = document.querySelector("#linkChooseClinic");
-chooseClinicButton.addEventListener("click", () => {
-  startPage.classList.add("hidden");
-  chooseCityPage.classList.remove("hidden");
-});
-
-function chosenCity() {
-  chooseCityPage.classList.add("hidden");
-  chooseHospitalPage.classList.remove("hidden");
+function handleSensor(e){
+  console.log(e.target.quaternion);
 }
-
-function chosenHospital() {
-  chooseHospitalPage.classList.add("hidden");
-  chooseDoctorPage.classList.remove("hidden");
-  track.classList.remove("hidden");
-}
-
-function chosenDoctor() {
-  chooseDoctorPage.classList.add("hidden");
-  trackPage.classList.remove("hidden");
-}
-
-track.addEventListener("click", () => {
-  trackPage.classList.add("hidden");
-  track.classList.add("hidden");
-  successPage.classList.remove("hidden");
-});
