d0f47cb92c45d32a60175304d3b277ccf575d5eb
NeonNygus <92753660+NeonNygus@users.noreply.github.com>
2023-03-24 14:08:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const x= document.querySelector("#x");
 const y= document.querySelector("#y");
-const sensor = new Accelerometer({frequency: 60});
-sensor.addEventListener("reading", (e) => 
-{handleSensor(e);
-    });
-sensor.start();
-
-function handleSensor(e){
-      console.log(e);
-      x.innerHTML = e;
-   // y.innerHTML = dist[1];
+let accelerometer = null;
+try {
+  accelerometer = new Accelerometer({ frequency: 10 });
+  accelerometer.onerror = (event) => {
+    // Handle runtime errors.
+    if (event.error.name === 'NotAllowedError') {
+      console.log('Permission to access sensor was denied.');
+    } else if (event.error.name === 'NotReadableError') {
+      console.log('Cannot connect to the sensor.');
+    }
+  };
+  accelerometer.onreading = (e) => {
+    console.log(e);
+  };
+  accelerometer.start();
+} catch (error) {
+  // Handle construction errors.
+  if (error.name === 'SecurityError') {
+    console.log('Sensor construction was blocked by the Permissions Policy.');
+  } else if (error.name === 'ReferenceError') {
+    console.log('Sensor is not supported by the User Agent.');
+  } else {
+    throw error;
+  }
 }
